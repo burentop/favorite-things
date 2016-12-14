82fbeb595989e1e72b9f40114368a0b5e3865e0f
Brent Perry <burentoperii@gmail.com>
2016-12-14 16:18:59 -0600

    collect user input into array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,32 @@
 
 
 $(document).ready(function () {
-  $(".lengths form").submit(function (event) {
+  $(".favorites form").submit(function (event) {
     event.preventDefault();
-    
-    
+    var thing1 = $("#thing1").val();
+    var thing2 = $("#thing2").val();
+    var thing3 = $("#thing3").val();
+    var things = [];
+
+    if (!thing1) {
+      $(".thing1").addClass("has-error");
+      $("#thing1-warning").text("Please enter your first favorite thing.");
+    }
+    if (!thing2) {
+      $(".thing2").addClass("has-error");
+      $("#thing2-warning").text("Please enter your second favorite thing.");
+    }
+    if (!thing3) {
+      $(".thing3").addClass("has-error");
+      $("#thing3-warning").text("Please enter your third favorite thing.");
+    }
+
+    if (thing1 && thing2 && thing3) {
+      things.push(thing1);
+      things.push(thing2);
+      things.push(thing3);
+    }
+
+    console.log(things);
   });
 });
